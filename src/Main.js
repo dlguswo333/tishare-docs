70b5a10002fb0ccfc1b7536d21baa790e89bba51
dlguswo333 <dlguswo333@skku.edu>
2021-08-02 15:19:52 +0900

    Complete basic Main

--- src/Main.js
+++ src/Main.js
@@ -5,8 +5,29 @@ function Main() {
     <div className={classes.Main}>
       <div className={classes.Heading}>
         <span>Share Files and Folder</span>
-        <span>with tiShare</span>
-        <img src='/desktop-run.png' alt='desktop-run'></img>
+        <span>with tiShare.</span>
+        <div className={classes.ImgContainer}>
+          <img src='/desktop-run.png' className={classes.DesktopSampleImg} alt='desktop-run' />
+          <img src='/mobile-run.png' className={classes.MobileSampleImg} alt='mobile-run' />
+        </div>
+      </div>
+      <div className={classes.Grid}>
+        <div className={classes.GridItem}>
+          <span className={classes.ItemTitle}>Share in Local Network</span>
+          <span className={classes.ItemBody}>Open yourself in Wi-Fi routers or Routers. Other devices will be able to see you.</span>
+        </div>
+        <div className={classes.GridItem}>
+          <span className={classes.ItemTitle}>Run Cross Platform</span>
+          <span className={classes.ItemBody}>Open yourself in Wi-Fi routers or Routers. Other devices will be able to see you.</span>
+        </div>
+        <div className={classes.GridItem}>
+          <span className={classes.ItemTitle}>Share both Files and Folders</span>
+          <span className={classes.ItemBody}>Open yourself in Wi-Fi routers or Routers. Other devices will be able to see you.</span>
+        </div>
+        <div className={classes.GridItem}>
+          <span className={classes.ItemTitle}>Multiple Tasks Support</span>
+          <span className={classes.ItemBody}>Open yourself in Wi-Fi routers or Routers. Other devices will be able to see you.</span>
+        </div>
       </div>
     </div>
   )
