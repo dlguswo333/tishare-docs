14aca0e110bfec6ea0485f8c95d4eac525f628f1
dlguswo333 <dlguswo333@skku.edu>
2021-08-04 12:03:59 +0900

    Add ErrorPage

---
+++ src/ErrorPage.js
@@ -0,0 +1,11 @@
+import classes from './ErrorPage.module.scss'
+
+function ErrorPage() {
+  return (
+    <div className={classes.ErrorPage}>
+      <span>404 Error.</span>
+    </div>
+  )
+}
+
+export default ErrorPage
