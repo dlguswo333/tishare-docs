9641c8f2ac542047625d5912f960810538e587a7
dlguswo333 <dlguswo002@gmail.com>
2022-10-30 13:26:35 +0900

    Move non-page codes back to src folder

---
+++ src/i18nInit.js
@@ -0,0 +1,28 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import enLang from '../public/lang/en.json'
+import koLang from '../public/lang/ko.json'
+
+function i18nInit() {
+  i18n.use(initReactI18next).init({
+    resources: {
+      en: {
+        translation: enLang
+      },
+      ko: {
+        translation: koLang
+      }
+    },
+    supportedLngs: [
+      'en', 'ko'
+    ],
+    fallbackLng: ['en', 'ko'],
+    lng: localStorage.getItem('preferredLang') || 'en',
+    debug: false
+  }, (err) => {
+    if (err)
+      console.log('i18n init error', err)
+  })
+}
+
+export default i18nInit
