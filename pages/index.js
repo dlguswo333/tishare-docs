8016362d3c3e694c96bcfcc439deefd8f5096485
dlguswo333 <dlguswo002@gmail.com>
2022-10-31 21:30:11 +0900

    Uninstall recoil dependency

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,10 @@
 import classes from './App.module.scss'
-import { RecoilRoot } from 'recoil'
-import { useEffect, useState } from 'react'
 import Home from './Home'
 
 function App() {
   return (
     <div className={classes.App}>
-      <RecoilRoot>
-        <Home />
-      </RecoilRoot>
+      <Home />
     </div>
   )
 }
